871f10e5498a2a88058f41b55cfd1bb1df0fb70f
08jne01 <joshnel123@gmail.com>
2018-03-22 02:40:19 +0000

    Uploading all Source

---
+++ mandelbrot/Header.h
@@ -0,0 +1,198 @@
+#pragma once
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <thread>
+#include <vector>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+
+#define pi 3.14159
+
+
+class complex
+
+{
+
+public:
+	double re;
+	double im;
+
+	void set(double a, double b)
+
+	{
+	
+		re = a;
+		im = b;
+
+	}
+
+	double mag()
+	
+	{
+
+		return (sqrt(re*re + im * im));
+
+	}
+
+	double arg()
+
+	{
+
+		double atan(im / re);
+
+	}
+
+	void complexSquare()
+
+	{
+		double a = re;
+		double b = im;
+
+		re = (a * a) - (b * b);
+		im = 2.0 * (a*b);
+
+	}
+
+	void add(complex r, complex g)
+
+	{
+
+		re = r.re + g.re;
+		im = r.im + g.im;
+
+	}
+
+};
+
+double inMandel(complex c)
+
+{
+
+	complex z_cur, z_prev;
+
+	z_prev.re = 0.0;
+	z_prev.im = 0.0;
+	for (int i = 0; i < 1000; i++)
+
+	{
+
+		z_prev.complexSquare();
+		z_cur.add(z_prev, c);
+
+		z_prev = z_cur;
+
+		//std::cout << z_prev.mag() << std::endl;
+
+		if (z_cur.re >= 2.0 || z_cur.re <= -2.0 || z_cur.im >= 2.0 || z_cur.im <= -2.0)
+
+		{
+
+			return (i) / (double)999.0;
+
+		}
+
+
+	}
+
+	return 1.0;
+
+}
+
+class mandel_class
+
+{
+
+public:
+		
+	std::vector<std::vector<complex>> cpx;
+	std::vector<std::vector<double>> colorArr;
+	double edgex;
+	double edgey;
+	double scale;
+	int size;
+
+	void calcMandel(int order, double ratio)
+
+	{
+		double progress;
+		double k = 2.0 / (order*scale);
+	
+		for (int i = 0; i < order; i++)
+
+		{
+			
+			std::thread mCalc
+
+			{
+
+				[&]()
+
+					{
+
+						threadCalc(i, order, ratio, k);
+
+					}
+
+			};
+
+			if (i < 2000)
+
+			{
+
+				mCalc.detach();
+
+			}
+
+			else
+
+			{
+
+				mCalc.join();
+
+			}
+
+			progress = 100.0*(i*i) / (order*order);
+			progress = round(progress);
+			std::cout.precision(0);
+			std::cout << "\rProgress:  " << progress << "% completed for res = " << order;
+	
+		}
+
+	}
+
+private:
+	
+	void threadCalc(int i, int order, double ratio, double k)
+
+	{
+		complex c;
+		double color;
+		std::vector<complex> buffer(order);
+		std::vector<double> bufcol(order);
+
+		double x0 = edgex + ((double)i * k);
+
+		for (int j = 0; j < ceil(order*ratio); j++)
+
+		{
+
+
+			c.set(x0, edgey + (double)j * k);
+
+
+			color = inMandel(c);
+
+			bufcol[j] = color;
+			buffer[j] = c;
+
+		}
+
+		cpx[i] = buffer;
+		colorArr[i] = bufcol;
+
+	}
+
+};
+
+
---
+++ mandelbrot/main.cpp
@@ -0,0 +1,516 @@
+#include "Header.h"
+
+static void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods);
+static void key_CallBack(GLFWwindow *window, int button, int scancode, int action, int mods);
+
+class program
+
+{
+
+private:
+	double step;
+	double ratio;
+	double phase;
+	double x;
+	double y;
+	double scale;
+	int greyscale;
+	int res;
+
+
+
+public:
+
+	mandel_class mandel;
+
+	double cursx;
+	double cursy;
+	double pointsize;
+	int supersample;
+	int width;
+	int height;
+	int fullscreen;
+
+	void init(int width_, int height_, double step_, double x_, double y_, double scale_, double greyscale_, double phase_, int fullscreen_)
+
+	{
+
+		width = width_;
+		height = height_;
+		step = step_;
+		x = x_;
+		y = y_;
+		scale = scale_;
+		pointsize = 1;
+		greyscale = greyscale_;
+		phase = phase_;
+		fullscreen = fullscreen_;
+		//supersample = supersample_;
+
+		ratio = height / (double)width;
+	
+		if (width > height)
+
+		{
+
+			res = width;
+
+		}
+
+		else if (height > width)
+
+		{
+
+
+			res = height;
+
+		}
+
+
+	}
+
+	void update(double local_step, int init)
+
+	{
+
+		ratio = height / (double)width;
+
+		int start_s = clock();
+
+		if (init != 1)
+
+		{
+
+			x += ((2.0 / (width*0.5))*cursx - 2.0) / scale;
+			y += ((-ratio * 2.0 / (height*0.5))*(cursy)+ratio * 2.0) / scale;
+
+		}
+
+
+		scale *= local_step;
+		mandel.scale = scale / 2.0;
+		mandel.edgex = -2.0 / scale + x;
+		mandel.edgey = -2.0*ratio / scale + y;
+
+
+		std::cout.precision(20);
+		std::cout << "\nx = " << x << std::endl;
+		std::cout << "y = " << y << std::endl;
+		std::cout.precision(1);
+		std::cout << "Zoom Level = " << scale << std::endl;
+
+		mandel.colorArr.resize(res*res);
+		mandel.cpx.resize(res*res);
+		mandel.calcMandel(res, ratio);
+		mandel.size = res;
+
+		int stop_s = clock();
+
+		std::cout << "\rRender Time: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << "                           " << std::endl;
+
+	}
+
+	void draw()
+
+	{
+
+		complex c;
+		long double q, r, g, b;
+
+		ratio = height / (double)width;
+
+		glBegin(GL_POINTS);
+
+
+		for (int i = 0; i < mandel.size; i++)
+
+		{
+
+			for (int j = 0; j < ceil(mandel.size*ratio); j++)
+
+			{
+
+				c = mandel.cpx[i][j];
+				q = 1 - mandel.colorArr[i][j];
+
+				if (greyscale >= 1)
+
+				{
+
+					r = q;
+					g = q;
+					b = q;
+
+				}
+
+				else
+
+				{
+
+					r = sin(2 * pi *q)*sin(2 * pi *q);
+					g = sin(phase* pi *q)*sin(2 * pi *q);
+					b = q;
+
+				}
+
+				glColor3d(r, g, b);
+				glVertex2d(((c.re) - x)*0.5*scale, ((c.im)  - y)*0.5*scale / ratio);
+
+				mandel.cpx[i][j];
+				mandel.colorArr[i][j];
+
+			}
+
+		}
+		glEnd();
+
+	}
+
+	void mouse_callback(GLFWwindow* window, int button, int action, int mods)
+
+	{
+
+		if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+
+		{
+
+			update(step, 0);
+
+		}
+
+		if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+
+		{
+
+			update(1.0 / step, 0);
+
+		}
+
+	}
+
+	void readConfig()
+
+	{
+
+		std::ifstream file("config.ini");
+
+		if (file.is_open())
+
+		{
+
+			file >> width;
+			file >> height;
+			file >> fullscreen;
+			file >> step;
+			file >> x;
+			file >> y;
+			file >> scale;
+			file >> greyscale;
+
+			if (greyscale != 1)
+
+			{
+
+				file >> phase;
+
+			}
+
+			file.close();
+
+		}
+
+		else
+
+		{
+
+			std::cout << "File could not be opened. Please run again and create config!" << std::endl;
+			system("PAUSE");
+			exit(EXIT_FAILURE);
+
+		}
+		
+		init(width, height, step, x, y, scale, greyscale, phase, fullscreen);
+
+
+	}
+
+	void writeConfig()
+
+	{
+
+		std::ofstream file("config.ini");
+
+		if (file.is_open())
+
+		{
+
+			file << width << std::endl;
+			file << height << std::endl;
+			file << fullscreen << std::endl;
+			file << step << std::endl;
+			file << x << std::endl;
+			file << y << std::endl;
+			file << scale << std::endl;
+			file << greyscale << std::endl;
+
+			if (greyscale != 1)
+
+			{
+
+				file.precision(20);
+				file << x << std::endl;
+
+			}
+
+			file.close();
+
+		}
+
+		else
+
+		{
+
+			std::cout << "File could not be opened." << std::endl;
+
+		}
+
+	}
+
+	void writeCoords()
+
+	{
+
+		std::ofstream file;
+
+		file.open("coords.txt", std::ios::app);
+
+		if (file.is_open())
+
+		{
+
+			file.precision(20);
+			file << "Re = " << x << " Im = " << y;
+			file.precision(3);
+			file << " Scale = " << scale << std::endl;
+			file.close();
+
+			std::cout << "\nCoords copied to file!" << std::endl;
+
+		}
+
+		else
+
+		{
+
+			std::cout << "File could not be opened." << std::endl;
+
+		}
+
+	}
+
+};
+
+
+int main()
+
+{
+
+	std::cout << "Mandelbrot Explorer, by Joshua Nelson." << std::endl;
+	std::cout << "Copyright Joshua Nelson 2018." << std::endl << std::endl;
+
+	program p;
+
+	int width, height, greyscale, newconfig, fullscreen;
+	double step, x, y, scale, phase;
+
+	//Input
+	std::cout << "Write New Config? (1 for yes 0 for no): ";
+	std::cin >> newconfig;
+
+	if (newconfig != 0)
+
+	{
+
+		std::cout << "\nWindow Width (500 - 2000 Pixels): ";
+		std::cin >> width;
+		std::cout << "\nWindow Height (500 - 2000 Pixels): ";
+		std::cin >> height;
+		//std::cout << "\nSuper Sample (experimental, 1 for off): ";
+		//std::cin >> supersample;
+		std::cout << "\nFull Screen (0 for windowed, 1 for fullscreen): ";
+		std::cin >> fullscreen;
+		std::cout << "\nZoom Step (2-20): ";
+		std::cin >> step;
+		std::cout << "\nInit Position x: ";
+		std::cin >> x;
+		std::cout << "\nInit Position y: ";
+		std::cin >> y;
+		std::cout << "\nInit Zoom: ";
+		std::cin >> scale;
+		std::cout << "\nGreyScale? (1 for grey scale 0 for color): ";
+		std::cin >> greyscale;
+
+		if (greyscale == 0)
+
+		{
+
+			std::cout << "\nColor Index (-2 - 2): ";
+			std::cin >> phase;
+
+		}
+
+		p.init(width, height, step, x, y, scale, greyscale, phase, fullscreen);
+		p.writeConfig();
+
+		if (step == 0 || scale == 0)
+
+		{
+
+			std::cout << "Divide by Zero Error please re-do the config more carefully!" << std::endl;
+			system("PAUSE");
+			exit(EXIT_FAILURE);
+
+		}
+
+	}
+	
+	else
+
+	{
+
+		p.readConfig();
+
+	}
+
+	if (!glfwInit())
+
+	{
+
+		exit(EXIT_FAILURE);
+
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	GLFWwindow* window;
+
+	if (p.fullscreen == 1)
+
+	{
+
+		window = glfwCreateWindow(p.width, p.height, "MandleBrot", glfwGetPrimaryMonitor(), NULL);
+
+	}
+
+	else
+
+	{
+
+		window = glfwCreateWindow(p.width, p.height, "MandleBrot", NULL, NULL);
+
+	}
+	
+
+	//glEnable(GL_MULTISAMPLE);
+
+	/*
+	if (p.supersample != 1)
+
+	{
+
+		glfwWindowHint(GLFW_SAMPLES, p.supersample);
+
+	}
+	*/
+
+	glfwSetWindowUserPointer(window, &p);
+
+	if (!window)
+
+	{
+
+		std::cout << "Print";
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	std::thread calc
+
+	{
+
+		[&]()
+
+		{
+
+			p.update(1.0, 1);
+
+		}
+
+	};
+
+	calc.detach();
+
+	//Render Thread
+	while (!glfwWindowShouldClose(window))
+
+	{
+		//Setup
+		glfwGetFramebufferSize(window, &p.width, &p.height);
+		glViewport(0, 0, p.width, p.height);
+		glDisable(GL_DEPTH_TEST);
+		glClearColor(1.0, 1.0, 1.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glPointSize(p.pointsize);
+
+		//Drawing
+
+		p.draw();
+
+		glfwGetCursorPos(window, &p.cursx, &p.cursy);
+		glfwSetMouseButtonCallback(window, mouseButtonCallback);
+		glfwSetKeyCallback(window, key_CallBack);
+
+		//Swap Buffer and Check Events
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate;
+	exit(EXIT_SUCCESS);
+
+}
+
+void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods)
+
+{
+
+	void* ptr = glfwGetWindowUserPointer(window);
+	program *kptr = static_cast<program*>(ptr);
+
+	kptr->mouse_callback(window, button, action, mods);
+
+}
+
+void key_CallBack(GLFWwindow *window, int button, int scancode, int action, int mods)
+
+{
+	
+	void* ptr = glfwGetWindowUserPointer(window);
+	program *kptr = static_cast<program*>(ptr);
+
+	if (button == GLFW_KEY_SPACE && action == GLFW_PRESS)
+
+	{
+
+		kptr->writeCoords();
+
+	}
+
+
+}
